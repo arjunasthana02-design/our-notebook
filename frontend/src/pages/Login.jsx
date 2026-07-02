1742a857fca162875de3470f07f22ef19b37cbcd
Arjun <arjunasthana02@gmail.com>
2026-07-02 18:22:14 +0530

    Fix login API

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
 
     try {
 
-      const res = await fetch("http://127.0.0.1:5000/login", {
+      const res = await fetch(apiUrl("/login"), {
 
         method: "POST",
 
