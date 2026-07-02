7b54972247faca923ceb679b371af01ed7b4f15c
Arjun <arjunasthana02@gmail.com>
2026-07-02 17:11:24 +0530

    Update CORS for production

--- backend/app.py
+++ backend/app.py
@@ -14,8 +14,7 @@ app = Flask(__name__)
 
 CORS(
     app,
-    resources={r"/*": {"origins": "http://localhost:5173"}},
-    supports_credentials=True
+    resources={r"/*": {"origins": "*"}}
 )
 
 # ==========================================
