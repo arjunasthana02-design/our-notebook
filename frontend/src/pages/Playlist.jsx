a071984ba2c088eead6a4998dd975524d4753bcc
Arjun <arjunasthana02@gmail.com>
2026-07-09 21:12:09 +0530

    Fix Spotify playlist embed

--- frontend/src/pages/Playlist.jsx
+++ frontend/src/pages/Playlist.jsx
@@ -3,7 +3,23 @@ import "./NotebookExtras.css";
 
 const SPOTIFY_PLAYLIST_URL =
   import.meta.env.VITE_SPOTIFY_PLAYLIST_URL ||
-  "https://open.spotify.com/embed/playlist/37i9dQZF1EJxsR4OUAMJeg";
+  "https://open.spotify.com/playlist/37i9dQZF1EJxsR4OUAMJeg?si=4e9d701a48684c15";
+
+function spotifyEmbedUrl(url) {
+  const fallbackId = "37i9dQZF1EJxsR4OUAMJeg";
+
+  try {
+    const spotifyUrl = new URL(url);
+    const playlistMatch = spotifyUrl.pathname.match(/\/(?:embed\/)?playlist\/([^/]+)/);
+    const playlistId = playlistMatch?.[1] || fallbackId;
+
+    return `https://open.spotify.com/embed/playlist/${playlistId}?utm_source=generator`;
+  } catch {
+    return `https://open.spotify.com/embed/playlist/${fallbackId}?utm_source=generator`;
+  }
+}
+
+const SPOTIFY_EMBED_URL = spotifyEmbedUrl(SPOTIFY_PLAYLIST_URL);
 
 export default function Playlist() {
   return (
@@ -20,7 +36,7 @@ export default function Playlist() {
         <div className="playlist-frame">
           <iframe
             title="Our Spotify Playlist"
-            src={SPOTIFY_PLAYLIST_URL}
+            src={SPOTIFY_EMBED_URL}
             width="100%"
             height="520"
             allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
